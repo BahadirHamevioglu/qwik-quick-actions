1c8a3f30bc893bb95d467bb6c49109df94b4b5fd
bahadirhamevioglu <hamevioglu.bahadirenes@hbv.edu.tr>
2024-01-28 23:47:25 +0300

    Fix formatting issues in QuickActions component and TransitionIf utility

--- src/components/quick-actions/quick-actions.tsx
+++ src/components/quick-actions/quick-actions.tsx
@@ -5,11 +5,11 @@ import {
   $,
   useOnWindow,
   useSignal,
-  useComputed$
+  useComputed$,
 } from "@builder.io/qwik";
 import Fuse from "fuse.js";
 
-import globalStyles from '@/assets/styles/main.scss?inline';
+import globalStyles from "@/assets/styles/main.scss?inline";
 import { Action } from "@/types/types";
 
 import TransitionIf from "../../utils/transition-if";
@@ -21,16 +21,16 @@ import styles from "./quick-actions.scss?inline";
 
 interface GroupFromProps {
   title: string;
-  actions: Omit<Action, 'index'>[];
+  actions: Omit<Action, "index">[];
 }
 
 interface Props {
   isOpen?: boolean;
-  animationType?: string;
+  animation?: "slide" | "none";
   actionGroups: GroupFromProps[];
 }
 
-const formatActionGroups = (actionGroups: Props['actionGroups']) => {
+const formatActionGroups = (actionGroups: Props["actionGroups"]) => {
   let currentIndex = 0;
 
   return {
@@ -39,127 +39,125 @@ const formatActionGroups = (actionGroups: Props['actionGroups']) => {
         ...group,
         actions: group.actions.map((action) => ({
           ...action,
-          index: currentIndex++
-        }))
+          index: currentIndex++,
+        })),
       };
     }),
-    maxIndex: currentIndex
+    maxIndex: currentIndex,
   };
 };
 
-export const QuickActions = component$<Props>(
-  (props) => {
-    useStyles$(globalStyles);
-    useStyles$(styles);
+export const QuickActions = component$<Props>((props) => {
+  useStyles$(globalStyles);
+  useStyles$(styles);
 
-    const focusedIndex = useSignal(2);
+  const focusedIndex = useSignal(2);
 
-    const formattedGroups = useComputed$(() => formatActionGroups(props.actionGroups));
+  const formattedGroups = useComputed$(() =>
+    formatActionGroups(props.actionGroups)
+  );
 
-    const input = useSignal<string>("");
-    const searchResults = useSignal<any[]>([]);
+  const input = useSignal<string>("");
+  const searchResults = useSignal<any[]>([]);
 
-    const isOpen = useSignal<boolean>(props.isOpen || false);
-    const animationType = useSignal<string>(props.animationType || "slide");
+  const isOpen = useSignal<boolean>(props.isOpen || false);
+  const animation = useSignal<string>(props.animation || "slide");
 
-    useOnWindow(
-      "keydown",
-      $((event) => {
-        if (event.metaKey && event.code === "KeyK") {
-          event.preventDefault();
-          isOpen.value = !isOpen.value;
-        }
-
-        if (!isOpen.value) return;
-
-        if (event.code === "Escape") {
-          event.preventDefault();
-
-          if (input.value.length > 0) {
-            input.value = "";
-          } else {
-            isOpen.value = false;
-          }
-        }
-
-        if (event.code === 'ArrowDown') {
-          focusedIndex.value = Math.min(focusedIndex.value + 1, formattedGroups.value.maxIndex - 1);
-        }
-
-        if (event.code === 'ArrowUp') {
-          focusedIndex.value = Math.max(focusedIndex.value - 1, 0);
-        }
-
-        if ([
-          'ArrowDown',
-          'ArrowUp'
-        ].includes(event.code)) {
-          setTimeout(() => {
-            const item = document.querySelector(`[data-index="${focusedIndex.value}"]`);
-
-            item?.scrollIntoView({
-              block: 'nearest',
-              inline: 'center'
-            });
-          });
-        }
-      })
-    );
-
-    useOnWindow(
-      "click",
-      $((event) => {
-        const target = event.target as HTMLElement;
-
-        if (!target.closest(".quick-actions") && isOpen.value) {
-          isOpen.value = false;
-        }
-      })
-    );
-
-    const onInput$ = $((event: InputEvent) => {
-      const target = event.target as HTMLInputElement;
-      input.value = target.value;
-
-      if (input.value.length === 0) {
-        searchResults.value = [];
-
-        return;
+  useOnWindow(
+    "keydown",
+    $((event) => {
+      if (event.metaKey && event.code === "KeyK") {
+        event.preventDefault();
+        isOpen.value = !isOpen.value;
       }
 
-      // const itemsExpanded = props.actionGroups.flatMap((item) => item.actions.map(action => ({
-      //   ...action,
-      //   parentLabel: item.title
-      // })));
+      if (!isOpen.value) return;
 
-      const fuse = new Fuse(props.actionGroups, {
-        keys: [
-          "title",
-          "actions.label"
-        ],
-        threshold: 0.2
-      });
+      if (event.code === "Escape") {
+        event.preventDefault();
 
-      const results = fuse.search(input.value);
+        if (input.value.length > 0) {
+          input.value = "";
+        } else {
+          isOpen.value = false;
+        }
+      }
 
-      searchResults.value = results;
+      if (event.code === "ArrowDown") {
+        focusedIndex.value = Math.min(
+          focusedIndex.value + 1,
+          formattedGroups.value.maxIndex - 1
+        );
+      }
+
+      if (event.code === "ArrowUp") {
+        focusedIndex.value = Math.max(focusedIndex.value - 1, 0);
+      }
+
+      if (["ArrowDown", "ArrowUp"].includes(event.code)) {
+        setTimeout(() => {
+          const item = document.querySelector(
+            `[data-index="${focusedIndex.value}"]`
+          );
+
+          item?.scrollIntoView({
+            block: "nearest",
+            inline: "center",
+          });
+        });
+      }
+    })
+  );
+
+  useOnWindow(
+    "click",
+    $((event) => {
+      const target = event.target as HTMLElement;
+
+      if (!target.closest(".quick-actions") && isOpen.value) {
+        isOpen.value = false;
+      }
+    })
+  );
+
+  const onInput$ = $((event: InputEvent) => {
+    const target = event.target as HTMLInputElement;
+    input.value = target.value;
+
+    if (input.value.length === 0) {
+      searchResults.value = [];
+
+      return;
+    }
+
+    // const itemsExpanded = props.actionGroups.flatMap((item) => item.actions.map(action => ({
+    //   ...action,
+    //   parentLabel: item.title
+    // })));
+
+    const fuse = new Fuse(props.actionGroups, {
+      keys: ["title", "actions.label"],
+      threshold: 0.2,
     });
 
-    return (
+    const results = fuse.search(input.value);
+
+    searchResults.value = results;
+  });
+
+  return (
+    <div>
       <TransitionIf
         class={[
           "quick-actions",
-          animationType.value ? `quick-actions-animation-${animationType.value}` : ''
+          animation.value ? `quick-actions-animation-${animation.value}` : "",
         ]}
         enter="quick-actions-animation-enter"
         exit="quick-actions-animation-exit"
         if={isOpen.value}
       >
         <div class="quick-actions-head">
-          <TextInput
-            value={input.value}
-            onInput$={onInput$}
-          />
+          <TextInput value={input.value} onInput$={onInput$} />
         </div>
         <div class="quick-actions-content">
           {/* No results, and no input */}
@@ -196,6 +194,6 @@ export const QuickActions = component$<Props>(
           })} */}
         </div>
       </TransitionIf>
-    );
-  }
-);
+    </div>
+  );
+});
