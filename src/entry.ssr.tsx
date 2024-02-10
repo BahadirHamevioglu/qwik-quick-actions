111fbfe84d44e523b92d63a3d55f3f1b476c6776
bahadirhamevioglu <hamevioglu.bahadirenes@hbv.edu.tr>
2024-02-10 23:53:35 +0300

    Update function to handle edge cases
    This commit updates the function to handle edge cases and improve error handling.

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -12,14 +12,15 @@
  */
 import {
   renderToStream,
-  type RenderToStreamOptions,
+  type RenderToStreamOptions
 } from "@builder.io/qwik/server";
 import { manifest } from "@qwik-client-manifest";
+
 import Root from "./root";
 
-export default function (opts: RenderToStreamOptions) {
+export default function(opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
-    ...opts,
+    ...opts
   });
 }
